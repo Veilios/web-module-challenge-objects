3940343e346177578bd31358404951856c50f684
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-06-25 20:42:05 -0400

    Created a function for task 1, but moved on to task 3 and completed that.

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+  const newItem = new Object();
+  newItem.name = name;
+  newItem.price = cost;
+  newItem.category = category;
+  return newItem;
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
-
+//createMenuItem("Taco", "4", "Lunch");
+//console.log(newItem);
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
@@ -40,6 +45,7 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 

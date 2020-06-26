7694abd20882bf2828d7d72376e570ff725e1085
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-06-25 21:17:21 -0400

    Completed task 7, creating a function that returns the last review of the reviews array

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function getReviewByIndex(reviews, index) {
   console.log(`${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`)
 }
 
-getReviewByIndex(reviews, 0);
+//getReviewByIndex(reviews, 0);
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
@@ -89,10 +89,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(reviews) {
+    let last = reviews[reviews.length - 1];
+    console.log(`${last.name} gave the restaurant a ${last.rating} star review and their feedback was: ${last.feedback}`)
   } 
 
+//getLastReview(reviews);
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
